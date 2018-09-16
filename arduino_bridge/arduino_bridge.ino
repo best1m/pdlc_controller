6f797f5ac8becb964b626d8eba1af561016ae411
kth <kthiswell@gmail.com>
2018-09-16 13:37:21 +0900

    edit arduino_bridge.ino

--- arduino_bridge/arduino_bridge.ino
+++ arduino_bridge/arduino_bridge.ino
@@ -1,20 +1,3 @@
-/**
- * Bridge script for Arduino Uno using SoftwareSerial
- * Created by Danila Loginov, December 23, 2016
- * https://github.com/1oginov/Cordova-Bluetooth-Terminal
- *
- * Wire bluetooth module to Arduino Uno as mentioned below, upload script, open Serial Monitor on PC and pair smartphone
- * with BT module. Open Terminal app, select paired device, connect to it and now whatever you send in Serial Monitor on
- * PC will appear in app and vice versa. Because of using SoftwareSerial this script is not reliable at all, some
- * symbols can be skipped, etc. So... buy Mega! Or STM32.
- *
- * Bluetooth (HC-05) to Arduino Uno wiring:
- * VCC - 5V
- * RX  - 3
- * TX  - 2
- * GND - GND
- */
-
 #define PC_BAUDRATE 9600
 #define BLUETOOTH_BAUDRATE 9600
 
