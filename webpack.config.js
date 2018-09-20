4b458dfddda32c10f260bc76908f3989d59db451
kthiswell <kthiswell@rocode.co.kr>
2018-09-20 15:42:52 +0900

    add sass module

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,37 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
-    entry : './www/js/index.js',
-    output : {
-        path : path.join(__dirname, '/www'),
-        filename : 'bundle.js'
+    entry: './www/js/index.js',
+    output: {
+        path: path.join(__dirname, '/www'),
+        filename: 'bundle.js'
     },
-    module : {
-        rules : [
-            {
-                test : /\.js$/,
-                exclude : /node_modules/,
-                use : {
-                    loader : 'babel-loader'
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                        loader: "style-loader" // creates style nodes from JS strings
+                    },
+                    {
+                        loader: "css-loader" // translates CSS into CommonJS
+                    },
+                    {
+                        loader: "sass-loader" // compiles Sass to CSS
+                    }
+                ]
             }
         ]
     },
-    plugins : [
+    plugins: [
         new HtmlWebpackPlugin({
-            template : './www/index.html',
+            template: './www/index.html',
             inject: false
         })
     ]
--- www/js/components/App.js
+++ www/js/components/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import bluetoothSerial from 'cordova-plugin-bluetooth-serial';
+import '../../scss/main.scss'
 
 class App extends Component {
     state = {
@@ -55,7 +56,7 @@ class App extends Component {
         const {listPairedDevices} = this.state;
         const lineStyle = {marginTop : '20px'}
         return (
-            <div>
+            <div className="main">
                 <h1>pdlc controller</h1>
                 <button onClick={this.callListPairedDevices}>LIST</button>
                 {listPairedDevices.length > 0
@@ -66,14 +67,14 @@ class App extends Component {
                         </div>
                     })
                     : ''}
-                <div style={lineStyle}>
+                <div className='kk-button-group'>
                     <button onClick={() => this.onSendData('a')}>TYPE A</button>
                     <button onClick={() => this.onSendData('b')}>TYPE B</button>
                     <button onClick={() => this.onSendData('c')}>TYPE C</button>
                     <button onClick={() => this.onSendData('d')}>TYPE D</button>
                 </div>
 
-                <div style={lineStyle}>
+                <div className='kk-button-group'>
                     <button onClick={this.onIncreaseCount}>INCREASE</button>
                     <button onClick={this.onDecreaseCount}>DECREASE</button>
                 </div>
